4ed732f8b0c71fa5e16dfd4e50d1d9a6c4f952ff
thutrang <thutrangdhbk@gmail.com>
2012-08-10 19:10:16 +0700

    Update NFC-based campus toolset/src/nfc/customview/ListItem.java

--- NFC-based campus toolset/src/nfc/customview/ListItem.java
+++ NFC-based campus toolset/src/nfc/customview/ListItem.java
@@ -8,17 +8,20 @@ public class ListItem {
 	private String sub_text;
 	private boolean isChecked;
 	private Long event_id;
-	
+	private Long startTime;
+	private Long endTime;
 	public ListItem (int imageURL, String main_text,String sub_text) {
 		this.imageURL=imageURL;
 		this.main_text=main_text;
 		this.sub_text = sub_text;
 	}
-	public ListItem (int imageURL, String place, String title, Long event_id) {
+	public ListItem (int imageURL, String place, String title, Long event_id, Long startTime, Long endTime) {
 		this.imageURL=imageURL;
 		this.main_text=place;
 		this.sub_text = title;
 		this.event_id = event_id;
+		this.startTime = startTime;
+		this.endTime = endTime;
 	}
 	
 	public int getImage() {
@@ -42,6 +45,14 @@ public class ListItem {
 
 	public Long getID(){
 		return event_id;
+	}
+	
+	public Long getStart(){
+		return startTime;
+	}
+	
+	public Long getEnd(){
+		return endTime;
 	}
 
 }
