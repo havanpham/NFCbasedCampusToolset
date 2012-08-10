eeec27cd9b57c23f82b1ba380232cd534d737db4
thutrang <thutrangdhbk@gmail.com>
2012-08-10 18:50:49 +0700

    Update NFC-based campus toolset/src/nfc/room/calendar/Calendar.java
    đã sửa hoàn thiện!

--- NFC-based campus toolset/src/nfc/room/calendar/Calendar.java
+++ NFC-based campus toolset/src/nfc/room/calendar/Calendar.java
@@ -1,53 +1,67 @@
 package nfc.room.calendar;
 
-import java.util.GregorianCalendar;
-
-import nfc.lib.LibMapActivity;
-
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.provider.CalendarContract.Calendars;
 import android.provider.CalendarContract.Events;
+import android.widget.Toast;
 
 public class Calendar extends Activity {
-	private static final String[] EVENT_PROJECTION = new String[] {
-	    Calendars._ID,                           // 0
-	    Calendars.ACCOUNT_NAME,                  // 1
-	    Calendars.CALENDAR_DISPLAY_NAME,         // 2
-	    Calendars.OWNER_ACCOUNT                  // 3
+
+		private String valueExtrasRoom;
+		private String valueExtrasHour;
+		private String[] hour1;
+		private String[] hour2;
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);
+			Bundle extras = this.getIntent().getExtras();
+			if (extras != null) {
+				valueExtrasRoom = extras.getString("Room");
+				valueExtrasHour = extras.getString("Hour");
 				
-		};
-	public static void Start(Context context){
-		Intent calIntent = new Intent(Intent.ACTION_INSERT);
-		calIntent.setData(CalendarContract.Events.CONTENT_URI);
-		context.startActivity(calIntent);
-
-		Intent intent = new Intent(Intent.ACTION_INSERT);
-		intent.setType("vnd.android.cursor.item/event");
-		intent.putExtra(Events.TITLE, "Learn Android");
-		intent.putExtra(Events.EVENT_LOCATION, "Home suit home");
-		intent.putExtra(Events.DESCRIPTION, "Download Examples");
-
-		// Setting dates
-		GregorianCalendar calDate = new GregorianCalendar(2012, 10, 02);
-		intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-				calDate.getTimeInMillis());
-		intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-				calDate.getTimeInMillis());
-
-		// Make it a full day event
-		intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
-
-		// Make it a recurring Event
-		intent.putExtra(Events.RRULE,
-				"FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");
-
-		// Making it private and shown as busy
-		intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);
-		intent.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
-
-		context.startActivity(intent);
-	}
+				String[] hour = valueExtrasHour.split("-");
+				Toast.makeText(this, hour[0], Toast.LENGTH_SHORT).show();
+				hour1 = hour[0].split(":");
+				hour2 = hour[1].split(":");
+				
+			}
+			
+			java.util.Calendar begin = java.util.Calendar.getInstance();
+			java.util.Calendar end = java.util.Calendar.getInstance();
+			
+			begin.set(java.util.Calendar.HOUR_OF_DAY, Integer.valueOf(hour1[0]));
+			begin.set(java.util.Calendar.MINUTE, Integer.valueOf(hour1[1]));
+			
+			if (Integer.valueOf(hour1[0]) <= 12){
+				begin.get(java.util.Calendar.AM);
+				
+			}
+			else {
+				begin.get(java.util.Calendar.PM);
+			}
+			
+			
+			end.set(java.util.Calendar.HOUR_OF_DAY, Integer.valueOf(hour2[0]));
+			end.set(java.util.Calendar.MINUTE, Integer.valueOf(hour2[1]));
+			
+			if (Integer.valueOf(hour2[0]) <= 12){
+				end.get(java.util.Calendar.AM);
+				
+			}
+			else {
+				end.get(java.util.Calendar.PM);
+			}
+			
+			Intent calIntent = new Intent(Intent.ACTION_INSERT);
+			calIntent.setData(CalendarContract.Events.CONTENT_URI);
+			calIntent.putExtra(Events.EVENT_LOCATION, valueExtrasRoom);
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin.getTimeInMillis());
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end.getTimeInMillis());
+			startActivity(calIntent);
+			finish();
+		}
 }
